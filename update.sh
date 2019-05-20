f552a0dfd96387a290ab7c00a22c9753f1698c8e
piggyvenus <piggyvenus@gmail.com>
2019-05-20 12:53:42 -0700

    script to build the lab content

---
+++ update.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git add .
+git commit -am "$(curl -s http://whatthecommit.com/index.txt 2>&1)"
+git push
+oc delete all --all -n labs
+oc new-app --name=guide samueltauil/workshopper -e CONTENT_URL_PREFIX="https://raw.githubusercontent.com/RedHatWorkshops/openshiftv4-workshop/master" -e WORKSHOPS_URLS="https://raw.githubusercontent.com/RedHatWorkshops/openshiftv4-workshop/master/_module_groups.yml"
