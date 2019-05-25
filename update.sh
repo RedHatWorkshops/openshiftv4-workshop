9969938d3cf12354960dc257acaf4322b5f22f3f
piggyvenus <piggyvenus@gmail.com>
2019-05-25 02:06:00 -0700

    add lab

--- update.sh
+++ update.sh
@@ -5,4 +5,4 @@ git commit -am "$(curl -s http://whatthecommit.com/index.txt 2>&1)"
 git push
 oc delete all --all -n labs
 oc new-app --name=guide samueltauil/workshopper -e CONTENT_URL_PREFIX="https://raw.githubusercontent.com/RedHatWorkshops/openshiftv4-workshop/master" -e WORKSHOPS_URLS="https://raw.githubusercontent.com/RedHatWorkshops/openshiftv4-workshop/master/_module_groups.yml"
-oc new-app --name=guide osevg/workshopper -e CONTENT_URL_PREFIX="https://raw.githubusercontent.com/RedHatWorkshops/openshiftv4-workshop/master" -e WORKSHOPS_URLS="https://raw.githubusercontent.com/RedHatWorkshops/openshiftv4-workshop/master/_module_groups.yml"
+oc new-app --name=dev-guide osevg/workshopper -e CONTENT_URL_PREFIX="https://raw.githubusercontent.com/RedHatWorkshops/openshiftv4-workshop/master" -e WORKSHOPS_URLS="https://raw.githubusercontent.com/RedHatWorkshops/openshiftv4-workshop/master/_module_groups.yml"
